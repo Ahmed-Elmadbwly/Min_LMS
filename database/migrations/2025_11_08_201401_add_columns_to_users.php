ab10cff81c88d11333a20380721c13fd93481071
Almadbwly <Madboly@gmail.com>
2025-11-14 19:07:05 +0200

    fix : migration of user

--- database/migrations/2025_11_08_201401_add_columns_to_users.php
+++ database/migrations/2025_11_08_201401_add_columns_to_users.php
@@ -17,6 +17,12 @@ return new class extends Migration
             $table->string('image')->nullable();
         });
 
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('12345678'),
+            'role' => 'admin',
+        ]);
     }
 
     /**
